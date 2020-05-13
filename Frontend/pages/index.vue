4e9bdcec26a031d81338a3751cc836da19cba646
KitaniIslam <slmkitani@gmail.com>
2020-05-13 13:42:43 +0100

    add search icon to search bar

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -4,16 +4,20 @@
       <h1>Search for your product Now !</h1>
       <p>easy search, add to basket in one click</p>
       <div class="search">
-        <a-input-group compact style="display: flex">
-          <a-select default-value="Option1">
-            <a-select-option value="Option1">
+        <a-input-group compact style="display: flex" >
+          <a-select default-value="Option1" >
+            <a-select-option value="Option1" >
               Option1
             </a-select-option>
             <a-select-option value="Option2">
               Option2
             </a-select-option>
           </a-select>
-          <a-input style/>
+          <a-input placeholder="Search ...">
+              <a-tooltip slot="suffix" title="Find product">
+                <a-icon type="search" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+          </a-input>
         </a-input-group>
       </div>
     </div>
