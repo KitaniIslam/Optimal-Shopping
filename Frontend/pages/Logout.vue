de0586bc180b54609a57febd698050e48627103e
KitaniIslam <slmkitani@gmail.com>
2020-05-13 01:57:20 +0100

    adding logout page view

---
+++ Frontend/pages/Logout.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+<script>
+export default {
+    async mounted() {
+    try {
+      await this.$auth.logout();
+    } finally {
+      this.$router.replace({ name: 'login' });
+    }
+  }
+}
+</script>
