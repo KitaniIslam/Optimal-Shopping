3d12fe624913e6f06e2ce3cae6c242d2f36ae735
KitaniIslam <slmkitani@gmail.com>
2020-05-15 01:50:34 +0100

    basket component

--- Frontend/components/Navbar/Basket/Card.vue
+++ Frontend/components/Navbar/Basket/Card.vue
@@ -1,3 +1,61 @@
 <template>
-    
+    <div class="item">
+        <div class="info-item">
+          <!-- <a-button  shape="circle" type="danger" icon="minus-circle" size="small" class="delete-button" /> -->
+          <a-icon type="minus-circle" :style="{color: 'red'}" class="delete-button" @click="deleteItem"/>
+          <p>Product</p>
+        </div>
+        <a-input-number id="inputNumber" v-model="quantity" size="small" :min="1" @change="onChange" />
+        <h3>{{total}} DZD</h3>
+    </div>
 </template>
+
+<script>
+export default {
+    data (){
+        return {
+            quantity : 1,
+            total : 170,
+            unitPrice: 170
+        }
+    },
+    methods: {
+        onChange(qVal){
+            this.total = this.unitPrice * this.quantity; 
+        },
+        deleteItem(){
+          // TODO: delete item from card list
+        }
+    }
+}
+</script>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-direction: row;
+  margin: 3px 0 3px 0;
+}
+
+.info-item {
+  display: flex;
+  flex-direction: row;
+  margin-right: 40px;
+  align-items: center;
+}
+
+.delete-button {
+  margin-right: 10px;
+}
+p {
+  margin: 0;
+}
+
+h3 {
+  margin: 0 0 0 10px;
+}
+
+.icon {
+  margin-left: 15px;
+}
+</style>
--- Frontend/components/Navbar/Basket/index.vue
+++ Frontend/components/Navbar/Basket/index.vue
@@ -1,24 +1,89 @@
 <template>
-  <a-dropdown>
+  <a-dropdown v-model="visible">
     <a-icon type="shopping-cart" :style="{ fontSize: '20px', color: '#fff'}" class="icon"/>
-    <a-menu slot="overlay">
-      <a-menu-item key="0">
-        <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+    <a-menu slot="overlay" @click="handleMenuClick">
+      <a-menu-item key="3">
+        <card />
       </a-menu-item>
       <a-menu-item key="1">
-        <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+        <card />
+      </a-menu-item>
+      <a-menu-item key="2">
+        <card />
       </a-menu-item>
       <a-menu-divider />
-      <a-menu-item key="3" disabled>
-        3rd menu item（disabled）
+      <a-menu-item key="0">
+        <div class="item">
+          <div class="sub-item">
+            <h3>Totale : <span> 510 DZD</span></h3>
+          </div>
+          <div class="sub-item">
+            <a-button icon="shopping-cart" size="small" type="primary">
+              Order
+            </a-button>
+          </div>
+        </div>
       </a-menu-item>
     </a-menu>
   </a-dropdown>
 </template>
 
 
+<script>
+import Card from './Card.vue';
+
+export default {
+  components :{
+    Card
+  },
+  data (){
+    return {
+      visible:false
+    }
+  },
+  methods: {
+    handleMenuClick(e){
+      if (e.key === '0') {
+        this.order();
+        this.visible = false;
+      }
+    },
+    order(){
+      this.$notification.open({
+        type:"success",
+        message: 'Your order has been sent',
+        description: 'You can Change/Cancel order in less than 1 hour.'
+      })
+    }
+  }
+}
+</script>
+
 <style scoped>
+.sub-item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
 .icon {
   margin-left:20px ;
 }
+.item {
+  display: flex;
+  flex-direction: row;
+  margin: 3px 0 3px 0;
+  justify-content: space-between;
+}
+
+
+p {
+  margin: 0;
+}
+
+h3 {
+  margin: 0;
+}
+
 </style>
