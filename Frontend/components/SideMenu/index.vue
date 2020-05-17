f870aae469c34cd1b66349c0a4faf34436a6490f
KitaniIslam <slmkitani@gmail.com>
2020-05-17 01:47:48 +0100

    active routes in sideMenu

--- Frontend/components/SideMenu/index.vue
+++ Frontend/components/SideMenu/index.vue
@@ -4,20 +4,34 @@
     <a-drawer title="MENU" placement="right" :closable="false" :visible="visible" @close="onClose">
       <a-menu>
         <a-menu-item>
-          <a-icon type="shopping-cart" />
-          Card
+          <nuxt-link to="/">
+            <a-icon type="home" />
+            Home
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item>
-          <a-icon type="inbox" />
-          Inbox
+          <nuxt-link to="/basket">
+            <a-icon type="shopping-cart" />
+            Card
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item>
-          <a-icon type="user" />
-          My Profile
+          <nuxt-link to="/inbox">
+            <a-icon type="inbox" />
+            Inbox
+          </nuxt-link>
         </a-menu-item>
         <a-menu-item>
-          <a-icon type="heart" />
-          About
+          <nuxt-link to="/profile">
+            <a-icon type="user" />
+            My Profile
+          </nuxt-link>
+        </a-menu-item>
+        <a-menu-item>
+          <nuxt-link to="/about">
+            <a-icon type="heart" />
+            About
+          </nuxt-link>
         </a-menu-item>
       </a-menu>
     </a-drawer>
