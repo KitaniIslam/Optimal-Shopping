088d284de804be5e8e51cb355ccf42f81900b113
KitaniIslam <slmkitani@gmail.com>
2020-05-28 16:24:16 +0100

    add default error page in case product is not selected (e.g. /products)

---
+++ Frontend/pages/products/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <a-empty :image="simpleImage" >
+            <span slot="description">OPS ! No Data .</span>
+            <nuxt-link to="/">
+                <a-button >
+                Select Product
+                </a-button>
+            </nuxt-link>
+        </a-empty>
+    </div>
+</template>
+
+<script>
+import { Empty } from 'ant-design-vue';
+export default {
+  beforeCreate() {
+    this.simpleImage = Empty.PRESENTED_IMAGE_SIMPLE;
+  },
+};
+</script>
+
+<style scoped>
+.container {
+    height: calc(100vh - 64px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
