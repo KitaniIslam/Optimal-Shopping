a1e957127aedc93d01cf345c10728743b4bbee62
KitaniIslam <slmkitani@gmail.com>
2020-05-30 17:39:58 +0100

    product details route

--- Frontend/pages/products/_product.vue
+++ Frontend/pages/products/_product.vue
@@ -1,3 +1,27 @@
 <template>
-    <p>im p !</p>
+    <div class="container">
+        <product-details />
+        <a-divider orientation="left" :style="{marginTop: '5rem'}">See also</a-divider>
+        <single-row />
+    </div>
 </template>
+
+<script>
+import ProductDetails from '~/components/ProductDetails';
+import SingleRow from '~/components/Products/SingleRow.vue'
+export default {
+    components: {
+        ProductDetails,
+        SingleRow
+    }
+    
+}
+</script>
+
+<style scoped>
+.container {
+    width: 70vw;
+    margin: 0 auto;
+    padding: 1rem 0 0 0 ;
+}
+</style>
