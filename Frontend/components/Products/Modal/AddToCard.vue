21196f56e2ca5d2c3e07bbcc2dfbf2d3ce2ba973
KitaniIslam <slmkitani@gmail.com>
2020-05-31 16:09:31 +0100

    add to card modal

---
+++ Frontend/components/Products/Modal/AddToCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <a-modal v-model="addToCardModal" title="Card Modal" @ok="handleOk" @cancel="cancel">
+      <p>{{product}}</p>
+    </a-modal>
+</template>
+
+<script>
+export default {
+    props: {
+        product : {
+            type: Object,
+            default: {
+                id: 0,
+                title: 'something',
+                price: 0
+            }
+        }
+    },
+    computed: {
+      addToCardModal(){return this.$store.getters['card/getModalVisible']}
+    },
+    methods: {
+        handleOk(){
+            this.$notification.open({
+                type:'success',
+                message: 'Added successfully',
+                description: ''
+            });
+
+            this.$store.commit('card/addToCard',{})
+            this.$store.commit('card/hideModal');
+        },
+        cancel(){
+            this.$store.commit('card/hideModal');
+        }
+    }
+}
+</script>
