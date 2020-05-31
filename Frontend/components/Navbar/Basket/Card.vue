9280a93e0eb6f6e1d1a2ba10c25c4fde78a2417f
KitaniIslam <slmkitani@gmail.com>
2020-05-31 16:12:42 +0100

    connect basket component (from navbar) to store (card store)

--- Frontend/components/Navbar/Basket/Card.vue
+++ Frontend/components/Navbar/Basket/Card.vue
@@ -1,60 +1,69 @@
 <template>
-    <div class="item">
-        <div class="info-item">
-          <a-icon type="minus-circle" :style="{color: 'red'}" class="delete-button" @click="deleteItem"/>
-          <p>Product</p>
-        </div>
-        <a-input-number  v-model="quantity" size="small" :min="1" @change="onChange" />
-        <h3>{{total}} DZD</h3>
+  <div class="item">
+    <div class="info-item">
+      <a-icon type="minus-circle" :style="{color: 'red'}" class="delete-button" @click="deleteItem" />
+      <p>Product</p>
     </div>
+    <a-input-number v-model="product.quantity" size="small" :min="1" @change="onChange" />
+    <h3>{{product.quantity * product.unitPrice}} DZD</h3>
+  </div>
 </template>
 
 <script>
-export default {
-    data (){
-        return {
-            quantity : 1,
-            total : 170,
-            unitPrice: 170
+  export default {
+    props: {
+      product: {
+        type: Object,
+        default:() => { return{
+          title: '',
+          originalPrice: 0,
+          unitPrice :0 ,
+          quantity: 1,
+          promotionApplied: false, 
+          id: 0}
         }
+      }
     },
     methods: {
-        onChange(qVal){
-            this.total = this.unitPrice * this.quantity; 
+      onChange(quantity) {
+        this.$store.commit('card/changeQuantity',{quantity:quantity,index:this.product.id});
         },
-        deleteItem(){
-          // TODO: delete item from card list
-        }
+      deleteItem() {
+        // TODO: delete item from card list
+      }
     }
-}
+  }
+
 </script>
 
 <style scoped>
-.item {
-  display: flex;
-  flex-direction: row;
-  margin: 3px 0 3px 0;
-}
+  .item {
+    display: flex;
+    flex-direction: row;
+    margin: 3px 0 3px 0;
+  }
 
-.info-item {
-  display: flex;
-  flex-direction: row;
-  margin-right: 40px;
-  align-items: center;
-}
+  .info-item {
+    display: flex;
+    flex-direction: row;
+    margin-right: 40px;
+    align-items: center;
+  }
 
-.delete-button {
-  margin-right: 10px;
-}
-p {
-  margin: 0;
-}
+  .delete-button {
+    margin-right: 10px;
+  }
 
-h3 {
-  margin: 0 0 0 10px;
-}
+  p {
+    margin: 0;
+  }
 
-.icon {
-  margin-left: 15px;
-}
+  h3 {
+    margin: 0 0 0 10px;
+  }
+
+  .icon {
+    margin-left: 15px;
+  }
+
 </style>
--- Frontend/components/Navbar/Basket/index.vue
+++ Frontend/components/Navbar/Basket/index.vue
@@ -1,88 +1,93 @@
 <template>
-<nuxt-link to="/basket">
-  <a-dropdown v-model="visible">
-    <a-icon type="shopping-cart" :style="{ fontSize: '20px', color: '#fff'}" />
-    <a-menu slot="overlay" @click="handleMenuClick">
-      <a-menu-item key="3">
-        <card />
-      </a-menu-item>
-      <a-menu-item key="1">
-        <card />
-      </a-menu-item>
-      <a-menu-item key="2">
-        <card />
-      </a-menu-item>
-      <a-menu-divider />
-      <a-menu-item key="0">
-        <div class="item">
-          <div class="sub-item">
-            <h3>Totale : <span> 510 DZD</span></h3>
+  <nuxt-link to="/basket">
+    <a-dropdown v-model="visible">
+      <a-icon type="shopping-cart" :style="{ fontSize: '20px', color: '#fff'}" />
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item v-for="item in products" :key="(item.id+1)">
+          <card :product="item" />
+        </a-menu-item>
+        <a-menu-item key="0">
+          <div class="item">
+            <div class="sub-item">
+              <h3>Totale : <span> {{totalPrice}}</span></h3>
+            </div>
+            <div class="sub-item">
+              <a-button icon="shopping-cart" size="small" type="primary">
+                Order
+              </a-button>
+            </div>
           </div>
-          <div class="sub-item">
-            <a-button icon="shopping-cart" size="small" type="primary">
-              Order
-            </a-button>
-          </div>
-        </div>
-      </a-menu-item>
-    </a-menu>
-  </a-dropdown>
-</nuxt-link>
+        </a-menu-item>
+      </a-menu>
+    </a-dropdown>
+  </nuxt-link>
 </template>
 
 
 <script>
-import Card from './Card.vue';
+  import Card from './Card.vue';
 
-export default {
-  components :{
-    Card
-  },
-  data (){
-    return {
-      visible:false
-    }
-  },
-  methods: {
-    handleMenuClick(e){
-      if (e.key === '0') {
-        this.order();
-        this.visible = false;
+  export default {
+    components: {
+      Card
+    },
+    data() {
+      return {
+        visible: false
       }
     },
-    order(){
-      this.$notification.open({
-        type:"success",
-        message: 'Your order has been sent',
-        description: 'You can Change/Cancel order in less than 1 hour.'
-      })
+    computed: {
+      products() {
+        return this.$store.getters['card/getCardContent'];
+      },
+      totalPrice() {
+        return this.products.reduce((sum, item) => {
+          return sum += (item.unitPrice * item.quantity)
+        }, 0);
+      }
+    },
+    mounted() {},
+    methods: {
+      handleMenuClick(e) {
+        if (e.key === '0') {
+          this.order();
+          this.visible = false;
+        }
+      },
+      order() {
+        this.$notification.open({
+          type: "success",
+          message: 'Your order has been sent',
+          description: 'You can Change/Cancel order in less than 1 hour.'
+        })
+      }
     }
   }
-}
+
 </script>
 
 <style scoped>
-.sub-item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
+  .sub-item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
 
-.item {
-  display: flex;
-  flex-direction: row;
-  margin: 3px 0 3px 0;
-  justify-content: space-between;
-}
+  .item {
+    display: flex;
+    flex-direction: row;
+    margin: 3px 0 3px 0;
+    justify-content: space-between;
+  }
 
 
-p {
-  margin: 0;
-}
+  p {
+    margin: 0;
+  }
 
-h3 {
-  margin: 0;
-}
+  h3 {
+    margin: 0;
+  }
 
 </style>
